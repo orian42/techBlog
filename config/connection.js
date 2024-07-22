76e55561f508a310a27885cf72e24e35cd595bbc
Jason Ryan <164967582+orian42@users.noreply.github.com>
2024-07-22 14:20:16 -0700

    reversed last

--- config/connection.js
+++ config/connection.js
@@ -8,9 +8,8 @@ const sequelize = process.env.DB_URL
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: process.env.DB_HOST || 'localhost',
+      host: 'localhost',
       dialect: 'postgres',
-      port: process.env.DB_PORT || 5432, 
     }
   );
 
